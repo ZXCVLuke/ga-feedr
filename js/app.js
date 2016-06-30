3360a68d9c1e78c110d5397ca0b95241c08c26e6
Luke <luke@zxcv.net.au>
2016-06-30 14:41:57 +1000

    Now pushing articles to an array of objects on the Feedr object to make the data available later. Phil helped to solve the context/closures double whammy.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,62 @@
 
-
+// Set up app object
 var Feedr = {
-  responseMethod : function(response) {
+  // Include objects for each news source
+  Mashable: {
+    // create array to fill with articles
+    articles: []
+  },
+  // create a function to handle the json response
+  responseMashable : function(response) {
+      // cponsole log json response
       console.log(response);
-
-      // response.new.forEach(function(result){
-      //   $("#main").append("<article class='article'>"+"<section class='articleContent'>"+"<h3>"+result.title+"<h3>"+"</section>"+"</article>")
-      // })
-
-
+      // something to do with closures... apparently the Feedr object isn't available inside the forEach function without the variable being defined here.
+      var self = this;
 
       response.new.forEach(function(result){
+
+        // push an object with all required article details for each article
+        self.Mashable.articles.push({
+          featuredImage: result.responsive_images[0].image,
+          articleLink: result.link,
+          articleTitle: result.title,
+          articleCategory: result.channel,
+          impressions: result.shares.total,
+          description: result.content.plain
+        });
+
+        // populate tamplate with article content
         var articleContents = { featuredImage: result.responsive_images[0].image,
                                 articleLink: result.link,
                                 articleTitle: result.title,
                                 articleCategory: result.channel,
-                                impressions: result.shares.total,
+                                impressions: result.shares.total
         };
-        var compiledTemplate = template(articleContents);
 
+        // complile and append template
+        var compiledTemplate = articleTemplate(articleContents);
         $("#main").append(compiledTemplate)
+
       })
 
+      // click function to test availability of data stored on articles array
+      $("#feedr").on("click", function() {
+        console.log(Feedr.Mashable.articles[0].description);
+      })
   }
 };
 
-var source = $('#article-template').html();
-var template = Handlebars.compile(source);
+
+// Set up Handlebars templates
+var template1 = $('#article-template').html();
+var articleTemplate = Handlebars.compile(template1);
+
+var template2 = $('#link-template').html();
+var linkTemplate = Handlebars.compile(template2);
 
 // ready DOM
 $(function() {
-
-  $.get('https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json', Feedr.responseMethod);
+  // get json feed from Mashable. heroku proxy required for CORS issue. Jquery proxy required to reset context from window to Feedr.
+  $.get('https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json', $.proxy(Feedr.responseMashable, Feedr));
 
 });
